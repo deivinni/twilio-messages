c66728e6978100b9614bdce32991b752ca17c51d
deivinni <deivinnimds3656@gmail.com>
2024-01-25 11:23:37 -0300

    add contentVariables

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import TwilioClient from "twilio";
-import { MessageInstance } from "twilio/lib/rest/api/v2010/account/message";
+import { MessageInstance, MessageListInstanceCreateOptions } from "twilio/lib/rest/api/v2010/account/message";
 
 const client = TwilioClient(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
@@ -8,7 +8,7 @@ export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<IMessageResponseData | IMessageErrorData | string>
 ) {
-  if (req.method != "POST") return res.status(400).send("This service only accepts POST method requests");
+  // if (req.method != "POST") return res.status(400).send("This service only accepts POST method requests");
 
   const url: URL = new URL(req.url as string, `https://${req.headers.host}`);
   const findParameter = (param: string): string => (url.searchParams.get(param)?.toString() as string);
@@ -18,6 +18,7 @@ export default async function handler (
   const from = findParameter("from");
   const to = findParameter("to");
   const contentSid = findParameter("content_sid");
+  const contentVariables = findParameter("variables");
 
   if (!from) return res.status(400).json({ error: { status: 400, message: "The \"from\" parameter is required" }});
   if (!to) return res.status(400).json({ error: { status: 400, message: "The \"to\" parameter is required" }});
@@ -28,15 +29,16 @@ export default async function handler (
   // #region create message
 
   const makeNumber = (phone: string) => "whatsapp:+" + phone.toString().replace(/[+]/g, '').trim();
+  let data: MessageListInstanceCreateOptions = {
+    from: makeNumber(from),
+    to: makeNumber(to),
+    contentSid,
+    messagingServiceSid: process.env.MESSAGING_SERVICE_SID
+  }
 
-  await client.messages.create(
-    {
-      from: makeNumber(from),
-      to: makeNumber(to),
-      contentSid,
-      messagingServiceSid: process.env.MESSAGING_SERVICE_SID
-    },
-    (error: Error, item: MessageInstance | undefined) => {
+  if (contentVariables) data = { ...data, contentVariables };
+
+  await client.messages.create(data, (error: Error, item: MessageInstance | undefined) => {
       if (error || typeof (item) === "undefined") {
         return res.status(500).send(error.message);
         // return res.status(500).json({ error: { code: 500, message: error.message } });
